8251e903e61b2da9b88ad7bc84e8a5313448b96f
clvnkhr <calvin_khor@hotmail.com>
2024-03-14 18:50:38 +0000

    vim mode

--- llllm.py
+++ llllm.py
@@ -15,12 +15,13 @@ OLLAMA_CONFIG = dict(
     keep_alive="5m",
     stream=False,
 )
-PROMPT_TEMPLATE = Template(
+FIX_PROMPT_TEMPLATE = Template(
     """Fix all typos, the casing, and the punctuation in the following text, \
 but preserve all the newline characters. \
 Do not modify LaTeX code or introduce escape characters. \
-Return only the corrected text, do not include a preamble and do not place the text in a code block:\
-    
+Return only the corrected text, do not include a preamble, \
+and do not place the text in a code block:\
+
 $text
 """
 )
@@ -34,7 +35,7 @@ $text
 
 
 def fix(text: str, client: httpx.Client) -> str:
-    prompt = PROMPT_TEMPLATE.substitute(text=text)
+    prompt = FIX_PROMPT_TEMPLATE.substitute(text=text)
     response = client.post(
         OLLAMA_ENDPOINT,
         json={"prompt": prompt, **OLLAMA_CONFIG},
@@ -47,50 +48,38 @@ def fix(text: str, client: httpx.Client) -> str:
 
 def fix_current_line(client: httpx.Client, vim_mode: bool = False) -> None:
     # on mac, select line by pressing cmd + shift + left
-    keys = [Key.cmd, Key.shift, Key.left]
-    _ = [controller.press(k) for k in keys]
-    _ = [controller.release(k) for k in keys]
+    if vim_mode:
+        _ = [controller.tap(k) for k in "0v$"]
+    else:
+        keys = [Key.cmd, Key.shift, Key.left]
+        _ = [controller.press(k) for k in keys]
+        _ = [controller.release(k) for k in keys]
     fix_selection(client)
 
 
-# def fix_selection(client: httpx.Client) -> None:
-#     with controller.pressed(Key.cmd):
-#         controller.tap("c")
-#     time.sleep(0.1)
-#     text = pyperclip.paste()
-#     pyperclip.copy(fix(text, client))
-#     time.sleep(0.1)
-#     with controller.pressed(Key.cmd):
-#         controller.tap("v")
 def fix_selection(client: httpx.Client, vim_mode: bool = False) -> None:
-    if vim_mode:
-        controller.tap("y")  # y = yank in visual mode
-    else:
-        with controller.pressed(Key.cmd):
-            controller.tap("c")
-    time.sleep(0.1)
+    def yank():
+        if vim_mode:
+            controller.tap("y")  # y = yank in visual mode
+        else:
+            with controller.pressed(Key.cmd):
+                controller.tap("c")
 
+    def paste():
+        if vim_mode:
+            # gvp = repeat previous selection then paste
+            _ = [controller.tap(k) for k in "gvp"]
+        else:
+            with controller.pressed(Key.cmd):
+                controller.tap("v")
+
+    yank()
+    time.sleep(0.1)
     text = pyperclip.paste()
     pyperclip.copy(fix(text, client))
     time.sleep(0.1)
+    paste()
 
-    if vim_mode:
-        # gvp = repeat previous selection then paste
-        _ = [controller.tap(k) for k in "gvp"]
-    else:
-        with controller.pressed(Key.cmd):
-            controller.tap("v")
-
-
-# if __name__ == "__main__":
-#     with httpx.Client(timeout=None) as client:
-#         with keyboard.GlobalHotKeys(
-#             {
-#                 str(Key.f9.value): (lambda: fix_selection(client)),
-#                 str(Key.f10.value): (lambda: fix_current_line(client)),
-#             },
-#         ) as h:
-#             h.join()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -109,7 +98,7 @@ if __name__ == "__main__":
         }
 
     with (
-        httpx.Client(timeout=None) as client,
-        keyboard.GlobalHotKeys(hotkeys=hotkeys(client)) as h,
+        httpx.Client(timeout=None) as c,
+        keyboard.GlobalHotKeys(hotkeys=hotkeys(c)) as h,
     ):
         h.join()
